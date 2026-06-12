17af60be0830efb5fb2959abe147e346e919d719
Daniel Villamizar <danieljosevillamizar@gmail.com>
2026-06-11 23:21:23 -0500

    chore: update Flutter SDK version to 3.44.2 in configuration files

--- .vscode/tasks/create_app.sh
+++ .vscode/tasks/create_app.sh
@@ -10,9 +10,9 @@ cd temp
 # Create a new flutter project
 fvm flutter create $1 --org $2
 
-# Ensure there is a directory with the app's name under projects/
+# Ensure there is a directory with the app's name
 cd ..
-mkdir -p projects/$1/
+mkdir -p ../$1/
 
 # Clean up any cruft that may be left behind in the template
 cd template/app
@@ -32,32 +32,32 @@ mv temp/template/app/web temp/
 rm -f temp/template/.env
 
 # Copy the files from the temporary template over to the application's project directory
-cp -R temp/template/. projects/$1/
-rm -rf projects/$1/packages/
-rm -rf projects/$1/supabase/
+cp -R temp/template/. ../$1/
+rm -rf ../$1/packages/
+rm -rf ../$1/supabase/
 
 # Copy over the directories we want from the flutter create command over to the project directory
-[ -d "temp/$1/android" ] && cp -R temp/$1/android projects/$1/app
-[ -d "temp/$1/ios" ] && cp -R temp/$1/ios projects/$1/app
-[ -d "temp/$1/web" ] && cp -R temp/$1/web projects/$1/app
+[ -d "temp/$1/android" ] && cp -R temp/$1/android ../$1/app
+[ -d "temp/$1/ios" ] && cp -R temp/$1/ios ../$1/app
+[ -d "temp/$1/web" ] && cp -R temp/$1/web ../$1/app
 
-rm -f projects/$1/app/web/index.html
-cp temp/web/index.html projects/$1/app/web/
-cp temp/web/flutter_bootstrap.js projects/$1/app/web/
+rm -f ../$1/app/web/index.html
+cp temp/web/index.html ../$1/app/web/
+cp temp/web/flutter_bootstrap.js ../$1/app/web/
 
-find projects/$1 -name '*.dart' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
-find projects/$1 -name '*.yaml' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
-find projects/$1 -name '*.md' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
-find projects/$1 -name '*.html' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
+find ../$1 -name '*.dart' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
+find ../$1 -name '*.yaml' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
+find ../$1 -name '*.md' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
+find ../$1 -name '*.html' -exec sed -i '' -e "s/gadfly_flutter_template/$1/g" {} \;
 
 # Now that we have changed the app names, we can safely copy in the packages directory
-cp -R temp/template/packages projects/$1/
+cp -R temp/template/packages ../$1/
 
-cp -R temp/template/supabase/migrations projects/$1/supabase/
-cp -R temp/template/supabase/functions projects/$1/supabase/
-cp temp/template/supabase/seed.sql projects/$1/supabase/
+cp -R temp/template/supabase/migrations ../$1/supabase/
+cp -R temp/template/supabase/functions ../$1/supabase/
+cp temp/template/supabase/seed.sql ../$1/supabase/
 
-cd projects/$1/app
+cd ../$1/app
 
 fvm flutter clean
 fvm flutter pub get
@@ -67,9 +67,9 @@ cd ..
 
 supabase init --with-vscode-settings
 
-cd ../..
+cd ../gadfly_flutter_template
 
-rm projects/$1/.vscode/extensions.json
-cp temp/template/.vscode/extensions.json projects/$1/.vscode/extensions.json
+rm ../$1/.vscode/extensions.json
+cp temp/template/.vscode/extensions.json ../$1/.vscode/extensions.json
 
 rm -rf temp
